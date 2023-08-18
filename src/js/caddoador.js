7dc084e1cc7055634f6d6d040a2c93465b87ed6e
Humberto Ribeiro <betofoxmal@gmail.com>
2023-08-17 23:02:39 -0300

    add Js no cadastro e edição do doador

---
+++ src/js/caddoador.js
@@ -0,0 +1,22 @@
+const clickCasa = document.getElementById('casa')
+const clickEdficio = document.getElementById('edificio')
+
+clickCasa.addEventListener('click', clicarCasa)
+clickEdficio.addEventListener('click', clicarEdificio)
+
+const ocutarEMostrar = document.getElementsByClassName("mostrar")
+
+function clicarEdificio() {
+    for (let classMostrar = 0; classMostrar < ocutarEMostrar.length; classMostrar++) {
+        const classAtual = ocutarEMostrar[classMostrar];
+        classAtual.classList.remove("ocutar")
+    }
+    
+}
+
+function clicarCasa() {
+    for (let classOcutar = 0; classOcutar < ocutarEMostrar.length; classOcutar++) {
+        const classAtual = ocutarEMostrar[classOcutar];
+        classAtual.classList.add("ocutar")
+    }
+}
