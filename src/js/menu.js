ce8b5af024613d6ecf445ea08b5af95ece9d2bd5
Humberto Sales <betofoxmal@gmail.com>
2023-11-18 21:23:02 -0300

    update js ajust

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,12 @@
 const menuBtnItens = document.querySelectorAll('.limenu');
 
+menuBtnItens.forEach((liitens) =>
+    liitens.addEventListener('click', selectMenu)
+);
+
 function selectMenu() {
     menuBtnItens.forEach((liitens) => 
         liitens.classList.remove('activa')
     );
     this.classList.add('activa');
 };
-
-menuBtnItens.forEach((liitens) =>
-    liitens.addEventListener('click', selectMenu)
-);
