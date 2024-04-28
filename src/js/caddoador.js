e479dbf1175ec2e2836497ac87c4b1eba0b7d062
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-04-28 17:43:58 -0300

    update seleção de casa/ap/empresa js

--- src/js/caddoador.js
+++ src/js/caddoador.js
@@ -1,23 +1,71 @@
 const clickCasa = document.getElementById('casa');
 const clickEdficio = document.getElementById('edificio');
+const clickEmpresa = document.getElementById('empresa');
+const selectCnpj = document.getElementsByClassName('cnpj');
+const selectEdificio = document.getElementsByClassName('edificio');
+const selectBloco = document.getElementsByClassName('bloco');
+const selectApartamento = document.getElementsByClassName('apartamento');
 
 clickCasa.addEventListener('click', clicarCasa);
 clickEdficio.addEventListener('click', clicarEdificio);
-
-const ocutarEMostrar = document.getElementsByClassName("mostrar");
-
-function clicarEdificio() {
-    for (let classMostrar = 0; classMostrar < ocutarEMostrar.length; classMostrar++) {
-        const classAtual = ocutarEMostrar[classMostrar];
-        classAtual.classList.remove("ocutar")
-    };
-    document.getElementById("cnpj").focus();
-};
+clickEmpresa.addEventListener('click', clicarEmpresa);
 
 function clicarCasa() {
-    for (let classOcutar = 0; classOcutar < ocutarEMostrar.length; classOcutar++) {
-        const classAtual = ocutarEMostrar[classOcutar];
+    for (let classOcutar = 0; classOcutar < selectCnpj.length; classOcutar++) {
+        const classAtual = selectCnpj[classOcutar];
+        classAtual.classList.add("ocutar")
+    };
+    for (let classOcutar = 0; classOcutar < selectEdificio.length; classOcutar++) {
+        const classAtual = selectEdificio[classOcutar];
+        classAtual.classList.add("ocutar")
+    };
+    for (let classOcutar = 0; classOcutar < selectBloco.length; classOcutar++) {
+        const classAtual = selectBloco[classOcutar];
+        classAtual.classList.add("ocutar")
+    };
+    for (let classOcutar = 0; classOcutar < selectApartamento.length; classOcutar++) {
+        const classAtual = selectApartamento[classOcutar];
         classAtual.classList.add("ocutar")
     };
     document.getElementById("pontoref").focus();
+};
+
+function clicarEdificio() {
+    for (let classMostrar = 0; classMostrar < selectEdificio.length; classMostrar++) {
+        const classAtual = selectEdificio[classMostrar];
+        classAtual.classList.remove("ocutar")
+    };
+    for (let classMostrar = 0; classMostrar < selectBloco.length; classMostrar++) {
+        const classAtual = selectBloco[classMostrar];
+        classAtual.classList.remove("ocutar")
+    };
+    for (let classMostrar = 0; classMostrar < selectApartamento.length; classMostrar++) {
+        const classAtual = selectApartamento[classMostrar];
+        classAtual.classList.remove("ocutar")
+    };
+    for (let classOcutar = 0; classOcutar < selectCnpj.length; classOcutar++) {
+        const classAtual = selectCnpj[classOcutar];
+        classAtual.classList.add("ocutar")
+    };
+    document.getElementById("predio").focus();
+};
+
+function clicarEmpresa() {
+    for (let classMostrar = 0; classMostrar < selectCnpj.length; classMostrar++) {
+        const classAtual = selectCnpj[classMostrar];
+        classAtual.classList.remove("ocutar")
+    };
+    for (let classMostrar = 0; classMostrar < selectEdificio.length; classMostrar++) {
+        const classAtual = selectEdificio[classMostrar];
+        classAtual.classList.remove("ocutar")
+    };
+    for (let classMostrar = 0; classMostrar < selectBloco.length; classMostrar++) {
+        const classAtual = selectBloco[classMostrar];
+        classAtual.classList.remove("ocutar")
+    };
+    for (let classMostrar = 0; classMostrar < selectApartamento.length; classMostrar++) {
+        const classAtual = selectApartamento[classMostrar];
+        classAtual.classList.remove("ocutar")
+    };
+    document.getElementById("cnpj").focus();
 };
